8b63cc834c374b47d8c7662145a9df1a764d59aa
Rodney Woollett <rodney.woollett@gmail.com>
2025-07-16 16:39:20 +1200

    git ignore

--- src/events/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './subjects';
-export * from './gameUpdateByIdEvent';
-export * from './gameCreateEvent';
-export * from './playerMoveEvent';
-
--- src/events/subjects.ts
+++
@@ -1,5 +0,0 @@
-export enum Subjects {
-  GameUpdateById = 'game_Update',
-  GameCreate = 'game_Create',
-  PlayerMove = 'player_Move',
-}
